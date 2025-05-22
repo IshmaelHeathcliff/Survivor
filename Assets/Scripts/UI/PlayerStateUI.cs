16ea5ff4e8201e0b887d9d7970b823884e634c95
Lawrence Heathcliff <darkflare1055@gmail.com>
2025-05-22 21:46:51 +0800

    art: 添加字体和基础UI
    属性数据UI
    血量和蓝量UI

---
+++ Assets/Scripts/UI/PlayerStateUI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Character.State
+{
+    [RequireComponent(typeof(StateUICellPool))]
+    public class PlayerStateUI : StateUI
+    {
+        readonly Dictionary<string, StateUICell> _stateUICells = new();
+        [SerializeField] StateUICellPool _pool;
+
+        async UniTaskVoid AddStateAsync(IState state)
+        {
+            StateUICell stateUICell = await _pool.Pop();
+
+            if (_stateUICells.ContainsKey(state.GetID()))
+            {
+                RemoveState(state.GetID());
+            }
+
+            _stateUICells.Add(state.GetID(), stateUICell);
+            stateUICell.InitStateUICell(state);
+        }
+
+        public override void AddState(IState state)
+        {
+            AddStateAsync(state).Forget();
+
+        }
+
+        public override void RemoveState(string id)
+        {
+            if (_stateUICells.Remove(id, out StateUICell stateUICell))
+            {
+                _pool.Push(stateUICell);
+            }
+        }
+
+        public override void ChangeStateTime(IStateWithTime state)
+        {
+            if (_stateUICells.TryGetValue(state.GetID(), out StateUICell stateUICell))
+            {
+                stateUICell.SetTime(state.TimeLeft, state.Duration);
+            }
+        }
+
+        public override void ChangeStateCount(IStateWithCount state)
+        {
+            if (_stateUICells.TryGetValue(state.GetID(), out StateUICell stateUICell))
+            {
+                stateUICell.SetCount(state.Count);
+            }
+        }
+
+        void OnValidate()
+        {
+            _pool = GetComponent<StateUICellPool>();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/StateUICellPool.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using Core;
+using Cysharp.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Character.State
+{
+    public class StateUICellPool : MonoBehaviour, IAsyncObjectPool<StateUICell>
+    {
+        [SerializeField] AssetReferenceGameObject _stateUICellReference;
+        [SerializeField] int _initialSize = 10;
+        [SerializeField] int _maxSize = 100;
+
+        AsyncOperationHandle<GameObject> _stateHandle;
+        readonly Stack<StateUICell> _pool = new();
+
+        public int Count => _pool.Count;
+
+        async UniTask<StateUICell> CreatObject()
+        {
+            await _stateHandle;
+            if (_stateHandle.Result == null) return null;
+
+            GameObject obj = Instantiate(_stateHandle.Result, transform);
+            obj.SetActive(false);
+            return obj.GetOrAddComponent<StateUICell>();
+        }
+
+        public async UniTask<StateUICell> Pop()
+        {
+            StateUICell stateUICell;
+            if (Count > 0)
+            {
+                stateUICell = _pool.Pop();
+            }
+            else
+            {
+                stateUICell = await CreatObject();
+            }
+
+            stateUICell.gameObject.SetActive(true);
+            return stateUICell;
+        }
+
+        public void Push(StateUICell obj)
+        {
+            obj.gameObject.SetActive(false);
+            if (Count > _maxSize)
+            {
+                Destroy(obj.gameObject);
+                return;
+            }
+            _pool.Push(obj);
+        }
+
+        async UniTaskVoid Init()
+        {
+            _stateHandle = Addressables.LoadAssetAsync<GameObject>(_stateUICellReference);
+
+            for (int i = 0; i < _initialSize; i++)
+            {
+                _pool.Push(await CreatObject());
+            }
+        }
+
+        void OnEnable()
+        {
+            Init().Forget();
+        }
+
+        void OnDisable()
+        {
+            Addressables.Release(_stateHandle);
+        }
+    }
+}
