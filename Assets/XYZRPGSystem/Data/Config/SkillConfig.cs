265a90b1fd2ce38499e6440850e0397edc541cc1
Arthur Heathcliff <darkflare1055@gmail.com>
2025-08-01 12:11:19 +0800

    improve: 技能配置添加合理的默认值

--- Assets/XYZRPGSystem/Data/Config/SkillConfig.cs
+++ Assets/XYZRPGSystem/Data/Config/SkillConfig.cs
@@ -37,7 +37,7 @@ namespace XYZRPGSystem.Data.Config
 
     public class RepetitiveSkillConfig : SkillConfig
     {
-        [ShowInInspector] public float Cooldown { get; set; }
+        [ShowInInspector] public float Cooldown { get; set; } = 1f;
         [ShowInInspector] public bool IsAutoUse { get; set; } = true;
     }
 
@@ -50,32 +50,32 @@ namespace XYZRPGSystem.Data.Config
     public class AttackSkillConfig : RepetitiveSkillConfig
     {
         [ShowInInspector] public List<AttackEffectConfig> AttackEffectConfigs { get; set; }
-        [ShowInInspector] public float Damage { get; set; }
-        [ShowInInspector] public float CriticalChance { get; set; }
-        [ShowInInspector] public float CriticalMultiplier { get; set; }
-        [ShowInInspector] public float AttackArea { get; set; }
-        [ShowInInspector] public float AttackRange { get; set; }
-        [ShowInInspector] public float AttackSpeed { get; set; }
-        [ShowInInspector] public float Duration { get; set; }
+        [ShowInInspector] public float Damage { get; set; } = 0f;
+        [ShowInInspector] public float CriticalChance { get; set; } = 0f;
+        [ShowInInspector] public float CriticalMultiplier { get; set; } = 150f;
+        [ShowInInspector] public float AttackArea { get; set; } = 1f;
+        [ShowInInspector] public float AttackRange { get; set; } = 4f;
+        [ShowInInspector] public float AttackSpeed { get; set; } = 1f;
+        [ShowInInspector] public float Duration { get; set; } = 3f;
 
-        [ShowInInspector] public float WoodOnUse { get; set; }
+        [ShowInInspector] public float WoodOnUse { get; set; } = 0f;
     }
 
     public class ProjectileAttackSkillConfig : AttackSkillConfig
     {
-        [ShowInInspector] public float ProjectileCount { get; set; }
-        [ShowInInspector] public float ProjectileSpeed { get; set; }
-        [ShowInInspector] public float ChainCount { get; set; }
-        [ShowInInspector] public float PenetrateCount { get; set; }
-        [ShowInInspector] public float SplitCount { get; set; }
-        [ShowInInspector] public bool IsTargetLocked { get; set; }
-        [ShowInInspector] public bool CanReturn { get; set; }
+        [ShowInInspector] public float ProjectileCount { get; set; } = 1f;
+        [ShowInInspector] public float ProjectileSpeed { get; set; } = 10f;
+        [ShowInInspector] public float ChainCount { get; set; } = 0f;
+        [ShowInInspector] public float PenetrateCount { get; set; } = 0f;
+        [ShowInInspector] public float SplitCount { get; set; } = 0f;
+        [ShowInInspector] public bool IsTargetLocked { get; set; } = false;
+        [ShowInInspector] public bool CanReturn { get; set; } = false;
     }
 
     public class SelfAttackSkillConfig : AttackSkillConfig
     {
-        [ShowInInspector] public bool CanReturn { get; set; }
-        [ShowInInspector] public bool IsTargetLocked { get; set; }
+        [ShowInInspector] public bool CanReturn { get; set; } = false;
+        [ShowInInspector] public bool IsTargetLocked { get; set; } = false;
 
     }
 }
